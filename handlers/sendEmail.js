26a76d43cab3444aa723a0ec3768ddf1a77fcf19
souvik mondal <38136428+KALYANIHF@users.noreply.github.com>
2025-09-29 20:50:47 +0530

    add the user route and authentication for the admin managemant

--- handlers/sendEmail.js
+++ handlers/sendEmail.js
@@ -1,4 +1,35 @@
-const sendEmail = (email, message, next) => {
+const nodemailer = require("nodemailer");
+const sendEmail = async (email, message) => {
   // send email using node mailer
+  const transpoter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+  const data = await transpoter.verify();
+  if (data) {
+    (async () => {
+      try {
+        const info = await transpoter.sendMail({
+          from: '"Example Team" <marve.sm.24@gmail.com>', // sender address
+          to: email, // list of receivers
+          subject: "Reset your password with auth token", // Subject line
+          text: "Check the below details to proceed", // plain text body
+          html: message, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      } catch (err) {
+        console.error("Error while sending mail", err);
+      }
+    })();
+  } else {
+    console.log("Error while verifying SMTP connection", data);
+  }
 };
 module.exports = sendEmail;
